e420a13ca95fd703e67542724fba24210ee91162
Adonias Goes <141375160+adoniasgoesw@users.noreply.github.com>
2026-05-29 11:32:52 -0300

    Fix casing of service image imports

--- client/src/data/Data.jsx
+++ client/src/data/Data.jsx
@@ -1,8 +1,8 @@
 import ServiceImage from "../assets/Service.jpeg";
 
-import ServiceImage01 from "../assets/service001.jpg";
-import ServiceImage02 from "../assets/service002.jpg";
-import ServiceImage03 from "../assets/service003.jpg";
+import ServiceImage01 from "../assets/Service001.jpg";
+import ServiceImage02 from "../assets/Service002.jpg";
+import ServiceImage03 from "../assets/Service003.jpg";
 import ServiceImage04 from "../assets/service004.jpg";
 
 
